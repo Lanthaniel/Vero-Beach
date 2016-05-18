651a0176f3dd7bb490e434ab1ecd14447674d916
Nate <natejoe@gmail.com>
2016-05-18 16:57:09 -0400

    Added more images to the assets and started a rough edit of the first view.

---
+++ vero-beach/MoreBtn.swift
@@ -0,0 +1,13 @@
+//
+//  MoreBtn.swift
+//  vero-beach
+//
+//  Created by Nate on 5/18/16.
+//  Copyright © 2016 Karabensh. All rights reserved.
+//
+
+import UIKit
+
+class MoreBtn: UIButton {
+
+}
